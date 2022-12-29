ec15af4962d8d039a3e26a782bf3ded85b90d5df
MiikaM <miika.mikkonen5@gmail.com>
2022-12-29 18:08:27 +0200

    11.9: fixed linting

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -8,10 +8,10 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-  it("Pokemon page can be opened and the correct pokemon is displayed", function () {
-    const venusaurPage = cy.contains("venusaur")
+  it('Pokemon page can be opened and the correct pokemon is displayed', function () {
+    const venusaurPage = cy.contains('venusaur')
     venusaurPage.click()
-    cy.get('[class="pokemon-ability-name"]:first').contains("overgrow")
-    cy.get('[class="pokemon-ability-name"]:last').contains("chlorophyll")
+    cy.get('[class="pokemon-ability-name"]:first').contains('overgrow')
+    cy.get('[class="pokemon-ability-name"]:last').contains('chlorophyll')
   })
 })
