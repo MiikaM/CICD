e1af80ea48441d9c53ccd441f94e4aed83921880
MiikaM <miika.mikkonen5@gmail.com>
2022-12-29 17:29:38 +0200

    11.9 test done

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,7 +1,17 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it("Pokemon page can be opened and the correct pokemon is displayed", function () {
+    const venusaurPage = cy.contains("venusaur")
+    venusaurPage.click()
+    cy.get('[class="pokemon-ability-name"]:first').contains("overgrow")
+    cy.get('[class="pokemon-ability-name"]:last').contains("chlorophyll")
   })
 })
