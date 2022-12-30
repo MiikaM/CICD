b9a5378baf4767b3ef7077867a810c0bac22680e
MiikaM <miika.mikkonen5@gmail.com>
2022-12-30 12:35:59 +0200

    11.11: removing health checks to see if frontend works

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-
-app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
-})
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 8080')
